3a29749c47ccb21ce9fa2871330bcf6763e903d3
jsmit257 <jsmit257@gmail.com>
2026-03-05 21:15:00 -0600

    basically works; needs more thorough testing (moved_to with no corresponding moved_from, files and directories)

--- createdb.sql
+++ createdb.sql
@@ -10,14 +10,12 @@ create table paths (
 	parentid integer     null references paths(id),
 	unique(name, parentid)
 );
-
 create index fk_paths_parentid on paths(parentid);
 
 create table files (
 	id    integer not null primary key references paths(id),
 	inode integer not null references inodes(inode)
 );
-
 create index fk_files_inode on files(inode);
 
 create table tagnames (
@@ -31,7 +29,6 @@ create table tagvalues (
 	inode     integer not null references inodes(inode),
 	tagnameid integer not null references tagnames(id)
 );
-
 create index fk_tagvalues_inode on tagvalues(inode);
 create index fk_tagvalues_tagnameid on tagvalues(tagnameid);
 
@@ -41,30 +38,46 @@ create table transients (
 	filetype char(1)  not null,
 	created  datetime not null default current_timestamp
 );
-
 -- transients is too small (usually empty) to need a foreign key index
 
-create view fullpaths 
+--
+-- build trees using every path as a tree root
+--
+create view allpaths
 as
-with recursive fp(id, fullpath, depth)
-as (
-	select  id
-	       ,name as fullpath
-				 ,0    as depth
-	  from  paths 
-	 where  parentid is null
+with recursive
+ap(start, end, path, depth) as (
+	select  id   as start
+	       ,id   as end
+	       ,name as path
+	       ,0    as depth
+	  from  paths
 	union all
-	select  p.id
-	       ,fp.fullpath || '/' || p.name
-				 ,fp.depth + 1
+	select  ap.start
+	       ,p.id
+	       ,ap.path || '/' || p.name
+	       ,ap.depth + 1
 	  from  paths p
-	  join  fp
-	    on  p.parentid = fp.id
+	  join  ap
+	    on  p.parentid = ap.end
 )
-select  id
-       ,fullpath
-			 ,depth
-  from  fp
+select  start
+       ,end
+       ,path
+       ,depth
+  from  ap
+;
+
+--
+-- just the tree with paths starting at the parent-less root
+--
+create view fullpaths
+as
+select  end as id
+       ,path as fullpath
+       ,depth
+  from  allpaths
+ where  start = 1
 ;
 
 create view tags
@@ -93,26 +106,48 @@ select  t.inode
 create view orphans
 as
 with recursive
-linked(pathid) as (
+--
+-- build paths from the leaves (files) down to the root
+--
+linked(parentid) as (
 	select  id
 	  from  files
 	union
-	select  parentid
-	  from  paths
-), unlinked(pathid) as (
-	select  p.id
-	  from  paths p
-	  left
-	  join  linked l
-	    on  p.id = l.parentid
-	 where  l.parentid is null
-	 union  all
-	select  p.id
-	  from  paths p
-	  join  unlinked u
-	    on  p.parentid = u.pathid
+	select  p.parentid
+	  from  linked l
+	  join  paths p
+	    on  l.parentid = p.id
 )
-select  pathid
-  from  unlinked
+--
+-- then select all the nodes that don't support a leaf
+--
+select  p.id
+  from  paths p
+  left
+  join  linked l
+    on  p.id = l.parentid
+ where  l.parentid is null
+;
+
+create view pruned
+as
+select  fp.fullpath as file
+       ,f.inode
+       ,count(links.id) as linkcount
+       ,t.created
+  from  transients t
+  join  allpaths   ap
+    on  t.oldpath  = ap.start
+  join  files      f
+    on  ap.end     = f.id
+  join  fullpaths  fp
+    on  f.id       = fp.id
+  left
+  join  files      links
+    on  f.inode    = links.id
+ group
+    by  fp.fullpath
+       ,f.inode
+       ,t.created
 ;
 
